4d0d091076dad37f9afe173e138547e6295b69cc
ioannis <canadiense@gmail.com>
2015-04-23 19:50:24 -0400

    modifying connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,7 +69,12 @@ module.exports = function (grunt) {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: 35729,
+        middleware: function(connect, options) {
+          return [function(req, res) {
+            require('fs').createReadStream('index.html').pipe(res);
+          }]
+        }
       },
       livereload: {
         options: {
