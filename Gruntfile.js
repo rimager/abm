33b43b4748c9a20dd38b0139fecbbf422f92daab
ioannis <canadiense@gmail.com>
2015-10-14 10:39:21 -0400

    fix gruntjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -447,7 +447,7 @@ module.exports = function (grunt) {
     grunt.registerTask('build', [
         'clean:dist',
         'wiredep',
-        'includeSource',fire
+        'includeSource',
         'useminPrepare',
         'concurrent:dist',
         'autoprefixer',
